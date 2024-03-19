af9530a2ef5ff52614214f3a042f6251a39f4937
Cy90sure <maria.pozniak2013@gmail.com>
2024-03-19 11:20:38 +0200

    Shit

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,13 +1,9 @@
 import os
 import sys
-from functions import add
 
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            '..')))
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from functions import add
 
 
 def test_add():
