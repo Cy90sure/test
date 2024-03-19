b17eb477d8857ec637fbc5b11e7daa6bb90c8d04
Cy90sure <maria.pozniak2013@gmail.com>
2024-03-19 11:09:08 +0200

    Added changes

---
+++ tests/test_add.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from functions import add
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..')))
+
+
+def test_add():
+    assert add(5, 5) == 10
--- tests/test_sum.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_sum():
-   assert sum(5, 5) == 10
